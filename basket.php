e914c1a492e4af16f4d559d4cd1d52f0baf91427
dis-zephyro <zephyro@yandex.ru>
2015-11-10 14:05:40 +0300

    v1

--- basket.php
+++ basket.php
@@ -39,25 +39,208 @@
                         <div class="basket">
                             <div class="basket__item">
                                 <div class="row">
-                                    <div class="col-md-4 col-lg-4">
-                                        <div class="basket__title">Состав</div>
-                                    </div>
-                                    <div class="col-md-8 col-lg-8">
-                                        <div class="row">
-                                            <div class="col-md-4 col-lg-4">
-                                                <div class="basket__title">Цена</div>
+                                    <div class="col-md-4 col-lg-5 clearfix">
+                                        <div class="basket__title text-left">Состав</div>
+                                        <div class="basket__image">
+                                            <img class="img-responsive" src="images/product_thumb_01.jpg" alt="">
+                                        </div>
+                                        <div class="basket__description">
+                                            <div class="basket__product">Платье 171081</div>
+                                            <div class="basket__color">
+                                                <span class="basket__small_title">Цвет:</span>
+                                                <span class="basket__color_value">Фиолетовый</span>
+                                                <span class="basket__color_image">
+                                                    <img src="images/colors/purple.png" alt="Фиолетовый">
+                                                </span>
                                             </div>
-                                            <div class="col-md-2 col-lg-2">
-                                                <div class="basket__title">Кол-во, шт.</div>
-                                            </div>
-                                            <div class="col-md-4 col-lg-4">
-                                                <div class="basket__title">Сумма</div>
-                                            </div>
-                                            <div class="col-md-2 col-lg-2">
-                                                <div class="basket__title">Удалить</div>
+                                            <div class="basket__size">
+                                                <span class="basket__small_title">Размер:</span>
+                                                <ul class="clearfix">
+                                                    <li><label><input type="radio" name="size">48</label></li>
+                                                    <li><label><input type="radio" name="size">50</label></li>
+                                                    <li><label><input type="radio" name="size" checked>52</label></li>
+                                                    <li><label><input type="radio" name="size">54</label></li>
+                                                    <li><label><input type="radio" name="size">56</label></li>
+                                                    <li><label><input type="radio" name="size">58</label></li>
+                                                </ul>
                                             </div>
                                         </div>
                                     </div>
+                                    <div class="col-md-8 col-lg-7">
+                                        <div class="row">
+                                            <div class="col-md-4 col-lg-4">
+                                                <div class="basket__title basket__title_column">Цена</div>
+                                                <div class="basket__col">
+                                                    <div class="basket__discount">Скидка: <span>20%</span></div>
+                                                    <div class="basket__price_old">5 000 руб.</div>
+                                                    <div class="basket__price_new">3 500 руб.</div>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-3 col-lg-3">
+                                                <div class="basket__title basket__title_column">Кол-во, шт.</div>
+                                                <div class="basket__col">
+                                                    <div class="basket__number number">
+                                                        <span class="minus"><i class="fa fa-plus"></i></span>
+                                                        <input type="text" value="1"/>
+                                                        <span class="plus"><i class="fa fa-minus"></i></span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-3 col-lg-3">
+                                                <div class="basket__title basket__title_column">Сумма</div>
+                                                <div class="basket__col"><span class="basket__summary">3 500 руб.</span> </div>
+                                            </div>
+                                            <div class="col-md-2 col-lg-2">
+                                                <div class="basket__title basket__title_column">Удалить</div>
+                                                <div class="basket__col">
+                                                    <span class="basket__delete">
+                                                        <i class="fa fa-times"></i>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="basket__item">
+                                <div class="row">
+                                    <div class="col-md-4 col-lg-5 clearfix">
+                                        <div class="basket__title text-left">Состав</div>
+                                        <div class="basket__image">
+                                            <img class="img-responsive" src="images/product_thumb_01.jpg" alt="">
+                                        </div>
+                                        <div class="basket__description">
+                                            <div class="basket__product">Платье 171081</div>
+                                            <div class="basket__color">
+                                                <span class="basket__small_title">Цвет:</span>
+                                                <span class="basket__color_value">Фиолетовый</span>
+                                                <span class="basket__color_image">
+                                                    <img src="images/colors/purple.png" alt="Фиолетовый">
+                                                </span>
+                                            </div>
+                                            <div class="basket__size">
+                                                <span class="basket__small_title">Размер:</span>
+                                                <ul class="clearfix">
+                                                    <li><label><input type="radio" name="size">48</label></li>
+                                                    <li><label><input type="radio" name="size">50</label></li>
+                                                    <li><label><input type="radio" name="size" checked>52</label></li>
+                                                    <li><label><input type="radio" name="size">54</label></li>
+                                                    <li><label><input type="radio" name="size">56</label></li>
+                                                    <li><label><input type="radio" name="size">58</label></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-8 col-lg-7">
+                                        <div class="row">
+                                            <div class="col-md-4 col-lg-4">
+                                                <div class="basket__title basket__title_column">Цена</div>
+                                                <div class="basket__col">
+                                                    <div class="basket__discount">Скидка: <span>20%</span></div>
+                                                    <div class="basket__price_old">5 000 руб.</div>
+                                                    <div class="basket__price_new">3 500 руб.</div>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-3 col-lg-3">
+                                                <div class="basket__title basket__title_column">Кол-во, шт.</div>
+                                                <div class="basket__col">
+                                                    <div class="basket__number number">
+                                                        <span class="minus"><i class="fa fa-plus"></i></span>
+                                                        <input type="text" value="1"/>
+                                                        <span class="plus"><i class="fa fa-minus"></i></span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-3 col-lg-3">
+                                                <div class="basket__title basket__title_column">Сумма</div>
+                                                <div class="basket__col"><span class="basket__summary">3 500 руб.</span> </div>
+                                            </div>
+                                            <div class="col-md-2 col-lg-2">
+                                                <div class="basket__title basket__title_column">Удалить</div>
+                                                <div class="basket__col">
+                                                    <span class="basket__delete">
+                                                        <i class="fa fa-times"></i>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="basket__item">
+                                <div class="row">
+                                    <div class="col-md-4 col-lg-5 clearfix">
+                                        <div class="basket__title text-left">Состав</div>
+                                        <div class="basket__image">
+                                            <img class="img-responsive" src="images/product_thumb_01.jpg" alt="">
+                                        </div>
+                                        <div class="basket__description">
+                                            <div class="basket__product">Платье 171081</div>
+                                            <div class="basket__color">
+                                                <span class="basket__small_title">Цвет:</span>
+                                                <span class="basket__color_value">Фиолетовый</span>
+                                                <span class="basket__color_image">
+                                                    <img src="images/colors/purple.png" alt="Фиолетовый">
+                                                </span>
+                                            </div>
+                                            <div class="basket__size">
+                                                <span class="basket__small_title">Размер:</span>
+                                                <ul class="clearfix">
+                                                    <li><label><input type="radio" name="size">48</label></li>
+                                                    <li><label><input type="radio" name="size">50</label></li>
+                                                    <li><label><input type="radio" name="size" checked>52</label></li>
+                                                    <li><label><input type="radio" name="size">54</label></li>
+                                                    <li><label><input type="radio" name="size">56</label></li>
+                                                    <li><label><input type="radio" name="size">58</label></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-8 col-lg-7">
+                                        <div class="row">
+                                            <div class="col-md-4 col-lg-4">
+                                                <div class="basket__title basket__title_column">Цена</div>
+                                                <div class="basket__col">
+                                                    <div class="basket__discount">Скидка: <span>20%</span></div>
+                                                    <div class="basket__price_old">5 000 руб.</div>
+                                                    <div class="basket__price_new">3 500 руб.</div>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-3 col-lg-3">
+                                                <div class="basket__title basket__title_column">Кол-во, шт.</div>
+                                                <div class="basket__col">
+                                                    <div class="basket__number number">
+                                                        <span class="minus"><i class="fa fa-plus"></i></span>
+                                                        <input type="text" value="1"/>
+                                                        <span class="plus"><i class="fa fa-minus"></i></span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-3 col-lg-3">
+                                                <div class="basket__title basket__title_column">Сумма</div>
+                                                <div class="basket__col"><span class="basket__summary">3 500 руб.</span> </div>
+                                            </div>
+                                            <div class="col-md-2 col-lg-2">
+                                                <div class="basket__title basket__title_column">Удалить</div>
+                                                <div class="basket__col">
+                                                    <span class="basket__delete">
+                                                        <i class="fa fa-times"></i>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="basket__buy text-right">
+                                <div class="basket__buy_discount">Ваша скидка: <span>2 500 руб.</span></div>
+                                <div class="basket__buy_summary">Сумма заказа:<span>12 000 руб.</span></div>
+                                <div class="row">
+                                    <a href="#" class="btn btn-mini">войти и заказать</a>
+                                    <a href="#" class="btn btn-mini">зарегистроваться и заказать</a>
                                 </div>
                             </div>
                         </div>
--- js/main.js
+++ js/main.js
@@ -253,3 +253,25 @@ $('.reply').tabs();
 $(".grade input[type='radio']").ionCheckRadio();
 $(".btn-recomend input[type='radio']").ionCheckRadio();
 $(".filter__group input[type='radio']").ionCheckRadio();
+$(".basket__size input[type='radio']").ionCheckRadio();
+
+
+
+// Minibasket
+
+$(document).ready(function() {
+    $('.minus').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.plus').click(function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
+});
